48b6d44e255be991260cb299f9c3c7bd4bb9bf80
Kathryn Stoddard <kathryn.stoddard@gmail.com>
2016-01-18 12:46:00 -0800

    Adding new images & styles for redesign. Updating to php

---
+++ footer.php
@@ -0,0 +1,16 @@
+<div id="footer">
+  <div class="container text-center">
+    <ul class="footer">
+      <li><a href="index.html">Home</a></li>
+        <li><a href="work.html">Work</a></li>
+        <li><a href="contact.html">Contact</a></li>
+        <li><a href="resume.pdf">Resume</a></li>
+        <li><a href="https://twitter.com/kathrynrefined" target="_blank"><img src="assets/images/social-twitter-white.png"></a></li>
+        <li><a href="https://github.com/kathrynstoddard" target="_blank"><img src="assets/images/social-github-white.png"></a></li>
+        <li><a href="http://www.linkedin.com/in/kathrynpdx/" target="_blank"><img src="assets/images/social-linkedin-white.png"></a></li>
+    </ul>
+  </div>
+</div><!-- End footer -->
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Kathryn Stoddard | Portfolio</title>
+
+	<!-- Bootstrap -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/main.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- favicon -->
+    <link rel="shortcut icon" href="assets/images/favicon.ico" />
+
+    <!-- Fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Lato:400,700,900' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+
+    <!-- jQuery  -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="assets/js/main.js"></script>
+
+</head>
+<body>
+
+	<!-- Fixed navbar -->
+    <div class="nav navbar-default navbar-fixed-top">
+      <div class="container">
+        <ul class="pull-left">
+          <li><a href="index.html"><img src="assets/images/ks.png"></a></li>
+        </ul>
+        <ul class="pull-right right-nav">
+        	  <li><a href="index.html">Home</a></li>
+            <li>&nbsp;//&nbsp;</li>
+          	<li><a href="work.html">Work</a></li>
+            <li>&nbsp;//&nbsp;</li>
+          	<li><a href="contact.html">Contact</a></li>
+            <li>&nbsp;//&nbsp;</li>
+          	<li><a href="resume.pdf">Resume</a></li>
+          	<li class="social-media"><a href="https://twitter.com/kathrynrefined" target="_blank"><img src="assets/images/social-twitter.png"></a></li>
+          	<li><a href="https://github.com/kathrynstoddard" target="_blank"><img src="assets/images/social-github.png"></a></li>
+          	<li><a href="http://www.linkedin.com/in/kathrynpdx/" target="_blank"><img src="assets/images/social-linkedin.png"></a></li>
+        </ul>
+      </div> <!-- End container -->
+    </div> <!-- End nav -->
